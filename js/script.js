d5675b199d0d5340ed314de592b024b37a06d6f7
Hernan.Reiq <hernandemure1202@gmail.com>
2021-01-22 18:26:23 -0400

    Diseño y animaciones terminado, web lista para publicarse

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    distance: '10px',
+    duration: 500,
+    origin: 'top'
+});
+sr.reveal('.img-portada', {
+    distance: '500px',
+    duration: 2000,
+    origin: 'left',
+    reset: true
+});
+sr.reveal('.text-portada', {
+    distance: '500px',
+    duration: 2000,
+    origin: 'right',
+    reset: true
+});
+sr.reveal('.contenedor-suscribirse', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.contenedor-proceo-libro', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.contenedor-pregunta', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.contenedor-acordeon', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.contenedor-equipo', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.contenedor-redes', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom'
+});
